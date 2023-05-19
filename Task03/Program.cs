7d46d8d2df3cc5705b669b6abf60083baf80169c
Nikita <nesterkov2000@mail.ru>
2023-05-19 16:05:49 +0700

    Pravki Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -5,14 +5,14 @@
 
 Console.WriteLine("Введите число от 1 до 7");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number <=7 && number>=1)
+if (number <= 7 && number >= 1)
 {
-    if (number ==1) Console.WriteLine("Понедельник");
-    if (number ==2) Console.WriteLine("Вторник");
-    if (number ==3) Console.WriteLine("Среда");
-    if (number ==4) Console.WriteLine("Четверг");
-    if (number ==5) Console.WriteLine("Пятница");
-    if (number ==6) Console.WriteLine("Суббота");
-    if (number ==7) Console.WriteLine("Воскресенье");
+    if (number == 1) Console.WriteLine("Понедельник");
+    else if (number == 2) Console.WriteLine("Вторник");
+    else if (number == 3) Console.WriteLine("Среда");
+    else if (number == 4) Console.WriteLine("Четверг");
+    else if (number == 5) Console.WriteLine("Пятница");
+    else if (number == 6) Console.WriteLine("Суббота");
+    else if (number == 7) Console.WriteLine("Воскресенье");
 }
 else Console.WriteLine("Неправильное число");
