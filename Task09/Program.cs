07fc59c4fc1a513f0d67502471f853b6dff5c591
Nikita <nesterkov2000@mail.ru>
2023-05-25 15:31:28 +0700

    Pravki Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -3,11 +3,18 @@
 // 12 -> 2
 // 85 -> 8
 
+//Первый способ
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 int num1 = number / 10;
 int num2 = number % 10;
-if (num1 > num2) 
+if (num1 > num2)
     Console.WriteLine($"Наибольшие цифра числа -> {num1}");
-else 
+else
     Console.WriteLine($"Наибольшие цифра числа -> {num2}");
+
+//Второй способ
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшие цифра числа -> {maxDigit}");
