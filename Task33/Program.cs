6d8541c911b800ddd671fff5e451ce6c3446f763
Nikita <nesterkov2000@mail.ru>
2023-06-18 16:38:48 +0700

    add Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand= new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(min,max+1);
+    }
+    return arr;
+}
+
+void PrintArray(int[]arr)
+{
+    int length= arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        if (length-i>1)
+            Console.Write($"{arr[i]}, ");
+        else 
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool ContainsNumber(int[]arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]==num) return true;
+        
+    }
+    return false;
+}
+
+int[] array=  CreateArray(10,-10,10);
+
+
+Console.Write("Введите число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+PrintArray(array);
+Console.WriteLine(ContainsNumber(array,number) ? $"Массив содержит число {number}" : $"Массив не содержит число {number}");
